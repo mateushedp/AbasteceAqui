aab36fccd88667a10629d35c8434a6da33920d50
Mateus Pacheco <mateushedlundp@gmail.com>
2022-07-03 13:23:59 -0300

    finalizados testes da station

--- controllers/stationController.js
+++ controllers/stationController.js
@@ -10,18 +10,6 @@ exports.getStations = async (req, res) => {
     }
 }
 
-
-// exports.getClient = async (req, res) => {
-//     const {id} = req.params;
-//     try {
-//         const client = await Client.findById({_id: id});
-//         res.json(client);
-//     } catch (error){
-//         console.log(error);
-//         res.json("Erro ao buscar!");
-//     }
-// }
-
 exports.saveStation = async (req, res) => {
     const station = req.body;
     const newStation = new Station({
@@ -38,26 +26,3 @@ exports.saveStation = async (req, res) => {
         res.json("Erro ao salvar!");
     }
 }
-
-// exports.updateClient = async (req, res) => {
-//     const {id} = req.params;
-//     const client = req.body;
-//     try {
-//         await Client.findOneAndUpdate({_id: id}, client);
-//         res.json("Editado com sucesso!");
-//     } catch (error){
-//         console.log(error);
-//         res.json("Erro ao editar!");
-//     }
-// }
-
-// exports.deleteClient = async (req, res) => {
-//     const {id} = req.params;
-//     try {
-//         await Client.findOneAndDelete({_id: id});
-//         res.json("Removido com sucesso!");
-//     } catch (error){
-//         console.log(error);
-//         res.json("Erro ao remover!");
-//     }
-// }
