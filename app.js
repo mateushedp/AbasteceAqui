44563bc8d84862ef26fb51ddd27ce29e3566d942
Mateus Pacheco <mateushedlundp@gmail.com>
2022-07-07 15:18:09 -0300

    limpando comentarios

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const express = require('express');
-//const cors = require('cors');
 const routes = require('./routes/routes');
 require('dotenv').config();
 
 const app = express();
-//app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-// mongoose
-// .connect(process.env.DB_CONN)
-// .then(() => {
-//     app.listen(process.env.PORT, () => {
-//         console.log("Ouvindo na " + process.env.PORT);
-//       })
-// })
-// .catch(error => {
-//     console.log(error);
-// });
 
 module.exports = app;
